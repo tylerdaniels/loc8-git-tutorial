fd9f8b5cd8ed84503c0380692ce1d0c8b7f3d972
Brett Pyke <bpyke@loc8.com>
2014-10-07 15:17:34 +1100

    added the feature to print out the name given in the first arguement

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@ public class Main
 {
     public static void main(String[] args)
     {
-	    System.out.println("Hello, " + Main.getWorldName());
+	    System.out.println("Hello, " + Main.getWorldName(args));
     }
 
-    private static String getWorldName()
+    private static String getWorldName(String[] args)
     {
-    	return "World";
+    	if (args == null || args.length == 0 || args[0].isEmpty())
+    		return "World";
+    	return args[0];
     }
 }

87f00922c5d2de1a45fc9b3e2969f811aba0d9c4
Brett Pyke <bpyke@loc8.com>
2014-10-07 15:08:28 +1100

    added the "view date" feature into the main application

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.util.Date;
+
 public class Main
 {
     public static void main(String[] args)
     {
 	    System.out.println("Hello, " + Main.getWorldName());
+	    System.out.println("Currently it is " + new Date().toString());
     }
 
     private static String getWorldName()
